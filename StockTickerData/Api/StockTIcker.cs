45e2d11035483edbb604a24bd5257bd063bfb69f
Prakash Dubey <prakash18d@gmail.com>
2021-06-09 23:32:13 +0530

    Add project files.

---
+++ StockTickerData/Api/StockTIcker.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using StockTickerData.Models;
+using StockTickerData.Shared;
+using System.Net;
+
+namespace StockTickerData.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockTIcker : ControllerBase
+    {
+        
+        private readonly IHttpClientFactory _clientfactory;
+        public StockTIcker(IHttpClientFactory clientfactory)
+        {
+            _clientfactory = clientfactory;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetTicker()
+        {
+            try
+            {
+                var client = _clientfactory.CreateClient("stock");
+                var response = await client.GetAsync("Quotes");//update here endpoint
+                var result = response.Content.ReadAsStringAsync().Result;
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.ToString());
+            }
+
+        }
+    }
+}
+
---
+++ StockTickerData/Models/StockDataModels.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockTickerData.Models
+{
+    public class StockDataModels
+    {
+      
+        public Links links { get; set; }
+        public Meta meta { get; set; }
+        public Datum[] data { get; set; }
+        public object error { get; set; }
+    }
+
+    public class Links
+    {
+        public string self { get; set; }
+    }
+
+    public class Meta
+    {
+        public DateTime executionDate { get; set; }
+        public int count { get; set; }
+    }
+
+    public class Datum
+    {
+        public string symbol { get; set; }
+        public string exchange { get; set; }
+        public string exchangeShortCode { get; set; }
+        public float lastTrade { get; set; }
+        public DateTime? date { get; set; }
+        public float changeNumber { get; set; }
+        public int changePercent { get; set; }
+        public float? bid { get; set; }
+        public int? bidSize { get; set; }
+        public int? askSize { get; set; }
+        public float dayHigh { get; set; }
+        public float dayLow { get; set; }
+        public int? volume { get; set; }
+        public float open { get; set; }
+        public float previousClose { get; set; }
+        public float _52WeekHigh { get; set; }
+        public DateTime? _52WeekHighDate { get; set; }
+        public float _52WeekLow { get; set; }
+        public DateTime? _52WeekLowDate { get; set; }
+        public float? peRatio { get; set; }
+        public long? marketCap { get; set; }
+        public int? sharesOutstanding { get; set; }
+        public float? rollingEPS { get; set; }
+        public string isDefault { get; set; }
+        public string isIndex { get; set; }
+        public string _class { get; set; }
+        public int securityId { get; set; }
+        public int? yield { get; set; }
+        public DateTime? firstTradeDate { get; set; }
+    }
+}
+
